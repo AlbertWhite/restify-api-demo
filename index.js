a6bffca453d7a6b050d2bfa7d342f82b2fc3cbf4
AlbertWhite <xyb1114@gmail.com>
2018-04-16 18:19:35 +0200

    update rules

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 var restify = require('restify')
 
+var httpGetResponse =  {email:{  
+         'validationRules':[  
+            {  
+               'errorMessage':'Vous devez renseigner votre adresse e-mail.',
+               'validationType':'required'
+            },
+            {  
+               'errorMessage':'Le format de votre adresse email n\'est pas valide.',
+               'validationParameters':{  
+                  'pattern':'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'
+               },
+               'validationType':'regex'
+            }
+         ],
+
+      },
+      'password':{  
+         'validationRules':[  
+            {  
+               'errorMessage':'Vous devez renseigner votre mot de passe.',
+               'validationType':'required'
+            },
+            {  
+               'errorMessage':'8 caractères minimum dont au moins 1 chiffre et 1 lettre',
+               'validationParameters':{  
+                  'pattern':'(?=^.{8,}$)(?=.*[a-zA-Z0-9]+)(?=.*\\d)(?=.*[a-zA-Z])[^ ]*$'
+               },
+               'validationType':'regex'
+            }
+         ]
+      }}
+
 function respondHttpGet(req, res, next) {
-  res.send('hello')
+  res.send(200, httpGetResponse)
   next()
 }
 
 function respondHttpPost(req, res, next) {
   console.log(req.params)
-  res.send('hello')
+  res.send(200, 'ok')
   next()
 }
 
 var server = restify.createServer()
 server.use(restify.plugins.bodyParser({ mapParams: true })) // in order to parse body, need to use plugin bodyParser
 
-server.get('/hello', respondHttpGet)
-server.post('/hello', respondHttpPost)
+server.get('/signup', respondHttpGet)
+server.post('/signup', respondHttpPost)
 
-server.listen(8080, function() {
+server.listen(8081, function() {
   console.log('%s listening at %s', server.name, server.url)
 })
